2f4e470bb88172d1d6740a78f5e5fa03bfada979
Divij Jain <jaindivijwork@gmail.com>
2022-11-03 20:10:30 +0530

    added the admin page

--- app/javascript/ijiel-frontend/src/app.tsx
+++ app/javascript/ijiel-frontend/src/app.tsx
@@ -7,16 +7,20 @@ import Archives from '@/pages/Archives';
 import Blogs from '@/pages/Blogs';
 import Submissions from '@/pages/Submissions';
 import About from '@/pages/About';
+import Admin from './pages/Admin';
 import NotFound from '@/pages/NotFound';
 
 const App = () => {
   return (
     <Routes>
+      {/* Main */}
       <Route path="/" element={<Homepage />} />
       <Route path="/archives" element={<Archives />} />
       <Route path="/blogs" element={<Blogs />} />
       <Route path="/submissions" element={<Submissions />} />
       <Route path="/about" element={<About />} />
+      {/* Admin */}
+      <Route path="/admin" element={<Admin />} />
       {/* Send all the other paths to notfound */}
       <Route path="*" element={<NotFound />} />
     </Routes>
---
+++ app/javascript/ijiel-frontend/src/pages/Admin/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Components
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
+
+const Admin = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>Admin</h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Admin;
